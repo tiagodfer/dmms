8c5f85053c1758a47ab43eb61846933bf6c9bac6
Tiago Dias Ferreira <51669608+tiagodfer@users.noreply.github.com>
2021-08-05 18:43:03 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+/**
+* Authors: Tiago Dias Ferreira and Marcello Silva Cruz
+*/
+
+/**
+* dmms: dynamic memory management system
+*/
+public class DMMS {
+    public class Program {
+        String name;
+        int priority;
+        int cpuCycles;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("DYNAMIC MEMORY MANAGEMENT SYSTEM");
+    }
+}
