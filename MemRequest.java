02d51af888c7491c3b9183534f212feed633a96e
Tiago Dias Ferreira <51669608+tiagodfer@users.noreply.github.com>
2021-08-11 17:19:21 -0300

    Update and rename memRequest.java to MemRequest.java

--- MemRequest.java
+++ MemRequest.java
@@ -1,10 +1,10 @@
 package SGDM;
 
-public class memRequest {
+public class MemRequest {
     private int id;
     private int size; //words
 
-    public memRequest(int id, int size) {
+    public MemRequest(int id, int size) {
         this.id = id; //talvez usar tipo AtomicInteger (java.util.concurrent.atomic) para automatizar o incremento
                       //https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/atomic/AtomicInteger.html
         this.size = size;
